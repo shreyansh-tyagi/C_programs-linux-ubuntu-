d71daa650346d0a6ac75bba1050d16e7a2e38d44
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-02-24 01:21:27 +0530

    trying to connect the database through c language

--- connect/connect.c
+++ connect/connect.c
@@ -6,4 +6,21 @@
 static char *host="localhost";
 static char *user="root";
 static *pass="mythemiz1524";
-static *db=
+static *db="operator";
+
+unsigned int port=3306;
+static char *unix_socket=NULL;
+unsigned int flag=0;
+
+int main()
+{
+MYSQL *conn;
+conn=mysql_init(NULL);
+if(!(mysql_real_connect(conn,host,user,pass,db,port,unix_socket,flag)))
+{
+fprintf(stderr,"\nError: %s [%d]\n", mysql_error(conn),mysql_errorno(conn));
+exit(1);
+}
+printf("shreyansh you are successfully connected to mysql server\n\n");
+return EXIT_SUCCESS;
+}
