7bb704856379a75304e1075e3284441f0c5bc8d5
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-05 02:38:07 +0530

    array reverse and delete the element from array has been added

--- array reverse and delete element from array/main.c
+++ array reverse and delete element from array/main.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+void reverse(int [],int);
+void delete(int[],int);
 void main()
 {
-    
-    printf();
-    printf();
-    printf();
+    int a[50],n,i;
+    printf("enter the size of array: ");
+    scanf("%d",&n);
+    printf("enter the value into the array: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    reverse(a,n);
+    delete(a,n);
+
 }
