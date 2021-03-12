f5958d83c9b594b9374fac7afeae0cd7b341d206
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-13 03:08:56 +0530

    some program oved into basic prgram on c folder

---
+++ some basic program of c/num.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the value of a and b\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("%d",c);
+}
+
---
+++ some basic program of c/pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("enter the value \n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("*");
+        
+        }
+        printf("\n");
+    }
+}
