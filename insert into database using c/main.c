85ff3b3984358fb08955004ec808c99ca35a5b54
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-21 00:24:41 +0530

    added mysql connection with c

--- insert into database using c/main.c
+++ insert into database using c/main.c
@@ -17,11 +17,9 @@ int main()
 MYSQL *conn;
 MYSQL_RES *res;
 MYSQL_ROW col;
-char name[50];
-int len = sizeof(name)+ 1;
-char * insert_query = (char *) malloc(len);
+int name;
 printf("\nenter the name: ");
-scanf("%s",name);
+scanf("%d",name);
 conn=mysql_init(NULL);
 if(!(mysql_real_connect(conn,host,user,pass,db,port,unix_socket,flag)))
 {
@@ -30,8 +28,7 @@ exit(1);
 }
 printf("\nHELLO SHREYANSH!----- you are now successfully connected to mysql server------\n\n");
 mysql_query(conn,"use operator");
-//mysql_query(conn,"insert into clothes values(name)");
-snprintf(insert_query, len, "INSERT INTO clothes VALUES(name)");
+mysql_query(conn,"insert into clothes (f_name) values('" + name + "')");
 
 mysql_query(conn,"select * from clothes");
 res=mysql_store_result(conn);
