727621302adfb3d82082596fcdcb6c824d08e7a6
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-09 00:01:08 +0530

    connectivity to database

--- connentivity/main.c
+++ connentivity/main.c
@@ -7,6 +7,9 @@ static char *host="localhost";
 static char *user="root";
 static char *pass="mythemiz1524";
 static char *db="operator";
+static char *unix_socket=NULL;
+unsigned int port=3306;
+unsigned int flag=0;
 void main()
 {
     
