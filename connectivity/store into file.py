6632e4981b26f49870075761678d39f20142613a
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-10 01:35:20 +0530

    store data into file

--- connectivity/store into file.py
+++
@@ -1,9 +0,0 @@
-myfile=open("data.txt",'w+')
-print('to exit from loop , type name "quit')
-while(True):
-    name=input('enter your name: ')
-    if name=='quit':
-        exit()
-    myfile.write(f'\nname={name}\n')
-    print('data saved into file')
-            
