77580fbf5312a46e6295370dd047e1c1640d35ea
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-10 01:49:36 +0530

    store data into file

--- connectivity/store into database using python.py
+++ connectivity/store into database using python.py
@@ -1,2 +1,3 @@
 import mysql.connector
-mydb=mysql.connector.connect(host='loaclhost',user='root',Pass='mythemiz1524',database='operator')
+mydb=mysql.connector.connect(host='loaclhost',user='root',Pass='mythemiz1524',database='operator',port=3306,auth_plugin='mysql_native_password')
+cursor=mydb.cursor()
