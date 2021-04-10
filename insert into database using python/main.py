7bedf7e34043646053530a41ed07d783f62e9a0b
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-10 19:00:58 +0530

    insert into database through python, c and mysql

--- insert into database using python/main.py
+++ insert into database using python/main.py
@@ -1,10 +1,16 @@
 import mysql.connector
 mydb=mysql.connector.connect(host='localhost',user='root',passwd='mythemiz1524',database='operator',auth_plugin='mysql_native_password')
 mycursor = mydb.cursor()
-sql = "INSERT INTO clothes (f_name) VALUES (%s)"
-val = ("John")
-mycursor.execute(sql,(val,))
+#sql = "delete from clothes where f_name=%s"
+count=0
+while(True):
+    name=input('enter the name to insert: ')
+    if name=='quit':
+        exit()
+    
+    sql="insert into clothes (f_name) values(%s) "    
+    mycursor.execute(sql,(name,)) 
+    count+=1
 
 mydb.commit()
-
-print(mycursor.rowcount, "record inserted.")
+print(count, "record inserted.")
