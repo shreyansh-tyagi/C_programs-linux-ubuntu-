1557282786492653cea3d3feb84f2612e20e405d
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-10 19:18:46 +0530

    insert into database using python

--- insert into database using python/main.py
+++ insert into database using python/main.py
@@ -2,15 +2,13 @@ import mysql.connector
 mydb=mysql.connector.connect(host='localhost',user='root',passwd='mythemiz1524',database='operator',auth_plugin='mysql_native_password')
 mycursor = mydb.cursor()
 #sql = "delete from clothes where f_name=%s"
+print('\n\nto exit , type name:-quit')
+a=int(input('enter how many records you want to insert: '))
 count=0
-while(True):
+for i in range(a):
     name=input('enter the name to insert: ')
-    if name=='quit':
-        exit()
-    
     sql="insert into clothes (f_name) values(%s) "    
     mycursor.execute(sql,(name,)) 
     count+=1
-
 mydb.commit()
-print(count, "record inserted.")
+print(f'{count},record inserted') 
