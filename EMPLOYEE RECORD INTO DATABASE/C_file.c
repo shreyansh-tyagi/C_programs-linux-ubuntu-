673aef87f9ea1e8a159d67852c5caad73ad1a4c1
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-04-13 23:53:46 +0530

    employee record into database has been added

--- EMPLOYEE RECORD INTO DATABASE/C_file.c
+++ EMPLOYEE RECORD INTO DATABASE/C_file.c
@@ -16,7 +16,7 @@ int main()
 {
     MYSQL *connection;
     MYSQL_RES *result;
-    MYSQL_ROW *row;
+    MYSQL_ROW *col;
     connection=mysql_init(NULL);
     if(!(mysql_real_connect(connection,host,user,pass,database,port,unix_socket,flags)))
     {
@@ -24,4 +24,15 @@ int main()
         exit(1);
     }
     printf("-----\nHELLO SHREYANSH, you are successfully connected to database-----\n");
+    mysql_query(connection,"use employee;");
+    mysql_query(connection,"select * from e_data;");
+    result=mysql_store_result(connection);
+    printf("\nID\t\tNAME\t\tADDRESS\t\tAGE\t\tGENDER\t\tSALARY\t\tDEPARTMENT\t\n\n");
+    while(col=mysql_fetch_row(result))
+    {
+        printf("%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\t%s\t\n\n",col[0],col[1],col[2],col[3],col[4],col[5],col[6]);
+    }
+    mysql_free_result(result);
+    mysql_close(connection);
+    return EXIT_SUCCESS;
 }
